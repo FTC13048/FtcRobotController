5062e559a881c9494e34631d83f4e53a77e3f089
MyCabbages4 <67510065+MyCabbages4@users.noreply.github.com>
2021-10-11 16:35:33 -0400

    bot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Robot {
+    private DcMotor FR, FL, BR, BL;
+    private HardwareMap map;
+    private Telemetry telemetry;
+
+    public Robot(HardwareMap hmap, Telemetry tele){
+        map = hmap;
+        telemetry = tele;
+    }
+
+    // initializes all key systems of the bot (motors, servos, etc.)
+    public void initBot(){
+        // Assign all motors to ports, name it this exact thing in the driver hub
+        BR = map.get(DcMotor.class, "back_right");
+        FR = map.get(DcMotor.class, "front_right");
+        BL = map.get(DcMotor.class, "back_left");
+        FL = map.get(DcMotor.class, "front_left");
+
+        // Set motor direction according to their orientation on the bot
+        //   motors on the left side will be reversed so that their directions coorespond to
+        //      the motors on the right
+        BR.setDirection(DcMotorSimple.Direction.FORWARD);
+        BL.setDirection(DcMotorSimple.Direction.REVERSE);
+        FL.setDirection(DcMotorSimple.Direction.REVERSE);
+        FR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    // Sets the strafing power for the robot (negative number will strafe left and positive
+    //     will strafe right)
+    public void strafe(double strafePower){
+        BR.setPower(-strafePower);
+        FR.setPower(strafePower);
+        BL.setPower(strafePower);
+        FL.setPower(-strafePower);
+    }
+
+    // Sets all motors to drive with the given power (positive forward negative backward)
+    public void drive(double rightStickY, double leftStickY){
+        BR.setPower(rightStickY);
+        FR.setPower(rightStickY);
+        BL.setPower(leftStickY);
+        FL.setPower(leftStickY);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTele.java
@@ -1,20 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.Hardware.*;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TankDrive", group="Teleop")
 
 public class TankTele extends OpMode {
 
-    // The motors that will be used
-    private DcMotor FL, FR, BL, BR;
+    private Robot bot;
 
     @Override
     // Initialize the robot (this is what happens when the play button is pressed)
     public void init() {
-        initBot();
+        bot = new Robot(hardwareMap, telemetry);
+        bot.initBot();
         telemetry.addData("Bot", "Initialized");
     }
 
@@ -42,65 +41,20 @@ public class TankTele extends OpMode {
 
         // if the right trigger is pressed down strafe at that power
         if(rightTrigger > 0.0){
-            strafe(rightTrigger);
+            bot.strafe(rightTrigger);
         } else if(leftTrigger > 0.0){
             // if the left trigger is pressed down, enter the negative value into the strafe
             //     function to make it strafe left
-            strafe(-leftTrigger);
+            bot.strafe(-leftTrigger);
         } else {
             // Otherwise, set the power to whatever the Y sticks are
-            FR.setPower(rightStickY);
-            BR.setPower(rightStickY);
-            FL.setPower(leftStickY);
-            BL.setPower(leftStickY);
+            bot.drive(rightStickY, leftStickY);
         }
     }
 
     @Override
     // Sets the power of all motors to 0 when stop button is pressed
     public void stop() {
-        FL.setPower(0.0);
-        FR.setPower(0.0);
-        BL.setPower(0.0);
-        BR.setPower(0.0);
-    }
-
-    // initializes all key systems of the bot (motors, servos, etc.)
-    private void initBot(){
-        // Assign all motors to ports, name it this exact thing in the driver hub
-        BR = hardwareMap.get(DcMotor.class, "back_right");
-        FR = hardwareMap.get(DcMotor.class, "front_right");
-        BL = hardwareMap.get(DcMotor.class, "back_left");
-        FL = hardwareMap.get(DcMotor.class, "front_left");
-
-        // Set motor direction according to their orientation on the bot
-        //   motors on the left side will be reversed so that their directions coorespond to
-        //      the motors on the right
-        BR.setDirection(DcMotorSimple.Direction.FORWARD);
-        BL.setDirection(DcMotorSimple.Direction.REVERSE);
-        FL.setDirection(DcMotorSimple.Direction.REVERSE);
-        FR.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-    // Sets the strafing power for the robot (negative number will strafe left and positive
-    //     will strafe right)
-    public void strafe(double strafePower){
-        BR.setPower(-strafePower);
-        FR.setPower(strafePower);
-        BL.setPower(strafePower);
-        FL.setPower(-strafePower);
-    }
-
-    // Sets all motors to drive with the given power (positive forward negative backward)
-    public void drive(double drivePower){
-        BR.setPower(drivePower);
-        FR.setPower(drivePower);
-        BL.setPower(drivePower);
-        FL.setPower(drivePower);
+        bot.drive(0.0, 0.0);
     }
 }
