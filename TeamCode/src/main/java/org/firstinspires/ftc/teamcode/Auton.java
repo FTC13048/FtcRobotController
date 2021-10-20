c41296e8e65604afca5cec53c6979a3d01bd35c1
Alex IP <20anip06@gmail.com>
2021-10-20 14:22:39 -0400

    Test to see if we can ease in the motors to prevents misalignment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -46,6 +47,8 @@ public class Auton extends OpMode {
     public void start() {
     }
 
+    int i = 0;
+
     @Override
     public void loop() {
         switch (test) {
@@ -53,9 +56,10 @@ public class Auton extends OpMode {
                 // the amount to turn
                 int turn = 254;
 
-                // if the heading is at or greater than the target stop the bot and reset the timer
+                // if the heading is at or greater than the target stop the bot
                 if (adjustHeading(turn, 0.5)) {
-                    bot.stop();
+                    i = 0;
+//                    bot.stop();
                     test++;
                     break;
                 }
@@ -83,16 +87,42 @@ public class Auton extends OpMode {
 
         // If the bot is within 30 degrees of the target, slow it down to 25% of the desired speed to prevent overshooting
         if (difference <= 30) {
-            bot.turn((power/4));
+            // Easing speed test
+            if (!bot.isEasingTurnSpeed) {
+                i = 0;
+                bot.atTargetTurnSpeed = false;
+            }
+            if (!bot.atTargetTurnSpeed)
+                bot.turnEased(power / 4, i);
+
+            i++;
         } else { // Otherwise use normal speed
-            bot.turn(power);
+            // Easing speed test
+            if (!bot.isEasingTurnSpeed) {
+                i = 0;
+                bot.atTargetTurnSpeed = false;
+            }
+            if (!bot.atTargetTurnSpeed)
+                bot.turnEased(power, i);
+
+            i++;
         }
+
 
         // If the bot is within 1 degree of the target, stop the bot and return true
         if (difference <= 1) {
             telemetry.addData("Stopping the ", "bot");
-            bot.stop();
-            return true;
+            // Easing to a stop test
+            if (!bot.isStopping) {
+                i = 0;
+                bot.stopped = false;
+            }
+            if (!bot.stopped)
+                bot.stopEased(i);
+            else
+                return true;
+
+            i++;
         }
 
         // return false otherwise
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Hardware;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Robot {
@@ -11,14 +12,21 @@ public class Robot {
     private Telemetry telemetry;
     private boolean isAuton;
 
-    public Robot(HardwareMap hmap, Telemetry tele, boolean auton){
+    public boolean stopped;
+    public boolean isStopping;
+    public boolean atTargetDriveSpeed;
+    public boolean isEasingDriveSpeed;
+    public boolean atTargetTurnSpeed;
+    public boolean isEasingTurnSpeed;
+
+    public Robot(HardwareMap hmap, Telemetry tele, boolean auton) {
         this.map = hmap;
         telemetry = tele;
         isAuton = auton;
     }
 
     // initializes all key systems of the bot (motors, servos, etc.)
-    public void initBot(){
+    public void initBot() {
         // Assign all motors to ports, name it this exact thing in the driver hub
         BR = map.get(DcMotor.class, "back_right");
         FR = map.get(DcMotor.class, "front_right");
@@ -44,7 +52,7 @@ public class Robot {
         FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        if(isAuton){
+        if (isAuton) {
             BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -54,7 +62,7 @@ public class Robot {
 
     // Sets the strafing power for the robot (negative number will strafe left and positive
     //     will strafe right)
-    public void strafe(double strafePower){
+    public void strafe(double strafePower) {
         BR.setPower(-strafePower);
         FR.setPower(strafePower);
         BL.setPower(strafePower);
@@ -62,24 +70,104 @@ public class Robot {
     }
 
     // Sets all motors to drive with the given power (positive forward negative backward)
-    public void drive(double rightStickY, double leftStickY){
+    public void drive(double rightStickY, double leftStickY) {
         BR.setPower(rightStickY);
         FR.setPower(rightStickY);
         BL.setPower(leftStickY);
         FL.setPower(leftStickY);
     }
 
-    public void turn(double power){
+    public void driveEased(double rightStickY, double leftStickY, int easeStage) {
+        setPowerEased(BR, rightStickY, easeStage, false);
+        setPowerEased(FR, rightStickY, easeStage, false);
+        setPowerEased(BL, leftStickY, easeStage, false);
+        setPowerEased(FL, leftStickY, easeStage, false);
+    }
+
+    public void turn(double power) {
         BR.setPower(-power);
         FR.setPower(-power);
         BL.setPower(power);
         FL.setPower(power);
     }
 
-    public void stop(){
+    public void turnEased(double power, double easeStage) {
+        setPowerEased(BR, -power, easeStage, true);
+        setPowerEased(FR, -power, easeStage, true);
+        setPowerEased(BL, power, easeStage, true);
+        setPowerEased(FL, power, easeStage, true);
+    }
+
+    public void stop() {
         BR.setPower(0.0);
         FR.setPower(0.0);
         BL.setPower(0.0);
         FL.setPower(0.0);
+    }
+
+    public void stopEased(double easeStage) {
+        BR.setPower(0.0);
+        FR.setPower(0.0);
+        BL.setPower(0.0);
+        FL.setPower(0.0);
+
+        setPowerEased(BR, 0.0, easeStage, false);
+        setPowerEased(FR, 0.0, easeStage, false);
+        setPowerEased(BL, 0.0, easeStage, false);
+        setPowerEased(FL, 0.0, easeStage, false);
+    }
+
+    // Ease in motor power by interpolating between the current power and the goal power using a stage variable
+    public void setPowerEased(Object motorObject, double goalPower, double easeStage, boolean isTurn) {
+        DcMotor motor = (DcMotor) motorObject;
+
+        if (motor != null) {
+            double currPower = motor.getPower();
+            double lerpedPower;
+            lerpedPower = lerpValue(currPower, goalPower, easeStage);
+
+            telemetry.addData(("goalPower for: " + motor.toString()), goalPower);
+            telemetry.addData(("currPower for: " + motor.toString()), currPower);
+            telemetry.addData(("easeStage for: " + motor.toString()), easeStage);
+            telemetry.addData(("lerpedPower for: " + motor.toString()), lerpedPower);
+            motor.setPower(lerpedPower);
+            if (lerpedPower == goalPower) {
+                stopped = false;
+                if (isTurn) {
+                    if (goalPower == 0) {
+                        isStopping = false;
+                        stopped = true;
+                    } else {
+                        isEasingTurnSpeed = false;
+                        atTargetTurnSpeed = true;
+                    }
+                } else {
+                    if (goalPower == 0) {
+                        isEasingDriveSpeed = false;
+                        stopped = true;
+                    } else {
+                        isEasingDriveSpeed = false;
+                        atTargetDriveSpeed = true;
+                    }
+                }
+            } else {
+                stopped = false;
+                if (isTurn) {
+                    isEasingTurnSpeed = true;
+                    atTargetTurnSpeed = false;
+                } else {
+                    if (goalPower == 0) {
+                        isStopping = true;
+                    } else {
+                        isEasingDriveSpeed = true;
+                        atTargetDriveSpeed = false;
+                    }
+                }
+            }
+        }
+    }
+
+    double lerpValue(double a, double b, double t) {
+        return (a * (1.0 - t)) + (b * t);
     }
 }
