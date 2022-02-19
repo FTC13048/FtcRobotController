2aa22844dbbae92a40dddb5c67b593d2a7d1d72f
MyCabbages4 <67510065+MyCabbages4@users.noreply.github.com>
2022-02-18 23:22:07 -0500

    vision added to boilerplate auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auton/BoilerplateAutonOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auton/BoilerplateAutonOpMode.java
@@ -8,6 +8,7 @@ import org.firstinspires.ftc.teamcode.Subsystems.DriveTrain;
 import org.firstinspires.ftc.teamcode.Subsystems.GamePadEx;
 import org.firstinspires.ftc.teamcode.Subsystems.Lift;
 import org.firstinspires.ftc.teamcode.Subsystems.RobotSubsystems;
+import org.firstinspires.ftc.teamcode.VisionStuff.VisionWrapper;
 
 /**
  * A boilerplate auton OpMode that can be used to make new auton OpModes that use subsystems
@@ -16,9 +17,14 @@ import org.firstinspires.ftc.teamcode.Subsystems.RobotSubsystems;
 //@Autonomous(name = "Subsystem Boilerplate Auton", group = "Subsystems") Uncomment this to make this OpMode useable
 public class BoilerplateAutonOpMode extends OpMode {
     private RobotSubsystems robot;
+    private VisionWrapper vision; // vision object for detection
+
+    private RobotSubsystems.DetectedLevel level;
+    private Lift.LiftLevel liftLevel;
 
     private AutonState state;
     private ElapsedTime timer;
+    private int one, two, three;
 
     public enum AutonState {
         STATE1, STATE2, STATE3,
@@ -30,18 +36,59 @@ public class BoilerplateAutonOpMode extends OpMode {
         robot = new RobotSubsystems(new GamePadEx(gamepad1), new GamePadEx(gamepad2), hardwareMap, telemetry, true);
         robot.init();
 
+        vision = new VisionWrapper(telemetry);
+        this.one = 0;
+        this.two = 0;
+        this.three = 0;
+
         timer = new ElapsedTime();
         state = AutonState.STATE1;
     }
 
     @Override
-    public void init_loop() {
-        // Run init loop code here
+    public void init_loop() { // init loop is always detection
+        // Get current detection every loop
+        this.level = this.vision.currentDetermination();
+        if (this.level != null) { // add one the level variable of whatever is detected each loop
+            // Add to value if detected
+            switch (this.level) {
+                case BOTTOM:
+                    this.one++;
+                    break;
+                case MIDDLE:
+                    this.two++;
+                    break;
+                case TOP:
+                    this.three++;
+                    break;
+            }
+
+            telemetry.addData("Current detected level: ", this.level);
+
+            telemetry.addLine("-------------------------------------");
+            telemetry.addLine("Overall detection numbers: (PRESS A TO RESET)");
+            telemetry.addData("LEVEL 1: ", this.one);
+            telemetry.addData("LEVEL 2: ", this.two);
+            telemetry.addData("LEVEL 3: ", this.three);
+
+            telemetry.update();
+        }
     }
 
     @Override
-    public void start() {
+    public void start() { // set the lift level to whatever is detected in init loop
         robot.driveTrain.startAuton();
+        switch(level){
+            case TOP:
+                liftLevel = Lift.LiftLevel.TOP;
+                break;
+            case MIDDLE:
+                liftLevel = Lift.LiftLevel.MID;
+                break;
+            case BOTTOM:
+                liftLevel = Lift.LiftLevel.BOT;
+                break;
+        }
     }
 
     @Override
