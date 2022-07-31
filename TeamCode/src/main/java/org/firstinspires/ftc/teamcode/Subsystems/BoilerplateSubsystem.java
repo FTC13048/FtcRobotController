4a87c9204d3564c0000e7df3a59e52b1c58af3a1
MyCabbages4 <67510065+MyCabbages4@users.noreply.github.com>
2022-07-31 18:22:37 -0400

    comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/BoilerplateSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/BoilerplateSubsystem.java
@@ -9,8 +9,12 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 /**
  * A boilerplate subsystem that you can use to quickly and easily create new subsystems
  *
+ * Each subsystem on the robot (i.e. the intake and the drivetrain) will have their own
+ * subsystem. This makes the code more modular so code can quickly be adapted if robot
+ * parts and systems are changed. It also makes the code cleaner in general.
  */
 public class BoilerplateSubsystem extends Subsystem {
+    // declare all hardware and any variables needed to change hardware speed or position
     private DcMotor motor;
     private final double motorPower = 0.5;
 
@@ -27,6 +31,8 @@ public class BoilerplateSubsystem extends Subsystem {
 
     @Override
     // This is where the system sends commands to the actual hardware based on the state
+
+    // ALL HARDWARE SHOULD BE MODIFIED IN THIS METHOD
     public void updateState() {
 
     }
@@ -35,6 +41,8 @@ public class BoilerplateSubsystem extends Subsystem {
     // This is where the system is told what inputs make it move states
     //     i.e. if the system is in STATE1 this is where it is told how it can
     //     move to STATE2
+
+    // NO HARDWARE SHOULD BE DIRECTLY MODIFIED IN THIS METHOD
     public void updateTeleOpState(GamePadEx gp1, GamePadEx gp2) {
 
     }
@@ -42,5 +50,10 @@ public class BoilerplateSubsystem extends Subsystem {
     @Override
     public void stop() {
 
+    }
+
+    // Each subsystem will need a state enum
+    public enum SubsystemState{
+        STATE1, STATE2
     }
 }
